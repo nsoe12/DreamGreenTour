d40037e6220d0b5f6b9632853ad04ba72d877113
BangJiSeok <bread0120@gmail.com>
2024-04-12 20:59:47 +0900

    Revert "no message"
    This reverts commit b028b57fa1d7a3a7044de0a9056e8ed321e7f5d7.

--- workspace/client/src/pages/airline/Airline.js
+++ workspace/client/src/pages/airline/Airline.js
@@ -1,46 +1,13 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
-import GlobalStyle from "../../global/global";
-import theme from "../../global/theme";
-import Layout from "../layout/Layout";
-import S from "./style";
-import TapComponent from "./TapComponent";
-import AirWeekSale from "./AirWeekSale";
+import { Outlet } from "react-router-dom";
 
 const Airline = () => {
-    return (
-        <ThemeProvider theme={theme}>
-            <GlobalStyle />
-            <Layout />
-
-            <S.AirHotelTabDiv>
-                <S.AirTabHeadingDiv>
-                    <S.AirTabHeading>항공</S.AirTabHeading>
-                </S.AirTabHeadingDiv>
-                <S.HotelTabHeadingDiv>
-                    <S.HotelTabHeading>호텔</S.HotelTabHeading>
-                </S.HotelTabHeadingDiv>
-            </S.AirHotelTabDiv>
-
-            <S.DivVisualRoll>
-                <S.Image1 src="/img/picture/slide_jeju.jpg" />
-                <S.Image2 src="/img/picture/slide_pohang.jpg" />
-                <S.Image3 src="/img/picture/slide_daegu.jpg" />
-            </S.DivVisualRoll>
-
-            <>
-                <TapComponent />
-            </>
-
-            <S.AirBanner1>
-                <S.AirBannerImage src="/img/airbanner.png"></S.AirBannerImage>
-            </S.AirBanner1>
-
-            <>
-                <AirWeekSale />
-            </>
-        </ThemeProvider>
-    );
+  return (
+    <div>
+      항공 페이지
+      <Outlet />
+    </div>
+  );
 };
 
 export default Airline;
